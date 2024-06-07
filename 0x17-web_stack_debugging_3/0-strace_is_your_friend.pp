2c07f47653fffcb79a19813a1f93e861904eeb65
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-06-07 03:25:48 +0300

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# This script looks onto the wordpress config file and replaces its bad value
+
+exec { 'replace_wrong':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
+  path    =>  '/bin:/usr/bin:/sbin:/usr/sbin:...'
+}
